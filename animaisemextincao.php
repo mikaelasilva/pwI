62b4bc445c6ad637a855c49d511e1b120dd51d1d
mikaela da silva <53869278+mikaelasilva@users.noreply.github.com>
2021-08-30 18:55:30 -0300

    Add files via upload

---
+++ animaisemextincao.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<link rel="shortcut icon" type="imagex/png" href="../../img/logo.png">
+<html lang="pt-br">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Animais em extinção</title>
+            <link rel="stylesheet" href="../estiloconteudo.css">
+        </head>
+
+<body>
+<!-- Barra superior de navegação -->
+
+<!-- Botao de voltar ao topo -->
+<span id='topo'></span>
+<!-- Botao de voltar ao topo -->
+
+        <div class="nav-container">
+					<a href="../../paginainicial.php">
+						<img id="logo" src="../../img/logo.png" alt="Little Body Big Brain">
+                       Little Body Big Brain
+					</a>
+					<i class="menuhorizontal"></i>
+					<ul>
+						<li><a href="../../conteudo.php">Matérias</a></li>
+						<li><a href="../../exercicios.php">Exercícios</a></li>
+						<li><a href="../../sobre.php">Sobre</a></li>
+                        <li><a href="../../helpme/help.php">Help me</a></li>
+					</ul>
+		</div class="nav-container">
+</br>
+
+
+
+<!-- Barra superior de navegação -->
+
+       
+    
+<!-- assunto -->
+        <div id="tela">
+
+            <div id="assunto">
+            <h1>animais em extinção</h1><hr />
+            
+            <a>diversos animais encontram-se atualmente, sob ameaça de extinção, e os motivos variam entre a destruição de seu habitat natural, mudanças climáticas, caça e desca descontroladas, entre outros fatores.</a>
+            </br></br><a>Animais ameaçados de extinção são aqueles em correm risco de desaparecerer de maneira definitiva do nosso planeta, ou seja, de tornar-se extintos. Apesar de ser relativamente comum na natureza, atualmente, o processo de extinção tem sido intensificado pelo homem.</a>
+            </br></br><a>Varias espécies vem sendo ameaçadas no mundo em decorrêcina da destruição do ambiente, mudanças climáticas, caça e pesca predatórias, poluição, entre outros fatores.</a>
+            </br></br><a>As espécies com um fator mais preocupante a respeito da provavel extinção estão classificados na Lista Vermelha da IUCN:</a>
+
+            <hr /><h2>Coala (Phascolarctos cinerus):</h2>
+            <p>os coalas estão classificados como vulneráveis.
+            </br></br>Atualmente, a população desses animas está diminuindo 
+            </p><hr />
+             <!-- como visualizar arquivo .php --> aaaaa
+
+
+
+             <hr /><h2>Baleia-azul (Baleanoptera musculus):</h2>
+            <p>os coalas estão classificados como vulneráveis.
+            </br></br>Atualmente, a população desses animas está diminuindo 
+            </p><hr />
+
+            <hr /><h2>Baleia-azul (Baleanoptera musculus):</h2>
+            <p>os coalas estão classificados como vulneráveis.
+            </br></br>Atualmente, a população desses animas está diminuindo 
+            </p><hr /> 
+
+
+
+
+
+
+
+            <hr /><h2>Sobre</h2>
+            <a>O Little Boddy Big Brain é um site feito como TCC, com o intuito de distribuir conteúdo educacional na internet. O site foi criado em 2021, com foco na ampliação e melhora dele ao longo prazo.</a><hr />
+
+               
+                   
+            <h2>Quem somos?</h2>
+            <a>Somos alunos da ETEC de Marília Antônio Devisate.
+            </br>Carlos Alberto
+            </br>Mikaela da Silva
+            </br>João Clemente</br><hr />
+            </a>
+            
+            </div id="assunto">
+
+            <div id="novidades">
+                <h3>Últimos contéudos</h3><hr />
+                <a href=".php"> Assunto </a></br>
+                <a href=".php"> Assunto </a></br>
+                <a href=".php"> Assunto </a></br>
+                <a href=".php"> Assunto </a></br>
+                <h3>Mais em assunto</h3><hr />
+                <a href=".php"> Assunto </a></br>
+                <a href=".php"> Assunto </a></br>
+                <a href=".php"> Assunto </a></br>
+                <a href=".php"> Assunto </a></br>
+                </br><hr /><a href="../biologia.php">Voltar para Biologia</a><hr />
+            </div id="novidades">
+        </div id="tela">
+</br>
+</br>
+</br>
+<!-- assunto -->
+
+
+
+
+
+
+
+<!-- Rodapé -->
+
+
+            <!-- Menu do Rodapé 
+            <div id="rodapeanterior">
+            
+            <a>Matematica </a></br>
+            <a>Matematica </a></br>
+            <a>Matematica </a></br>
+            <a>Matematica </a></br>
+
+            </div id="rodapeanterior">
+             Menu do Rodapé -->
+                    <div id="rodape">
+                        <footer>
+                            Projeto realizado pelos alunos da ETEC <article>
+                        </footer>
+
+                        
+                    </div id="rodape">
+                    
+                    <!-- Botao de voltar ao topo -->
+                    <div id="botaotopo">
+                    <a href='#topo'>Voltar ao topo</a>
+                    <!-- Botao de voltar ao topo -->
+                    </div id="botaotopo">
+<!-- Rodapé -->
+
+</body>
+</html>
